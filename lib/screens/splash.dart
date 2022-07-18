0d4deb7bcec11cec64856c9ccd994d6ef8c917d5
Andraž Anderle <andraz.anderle@gmail.com>
2022-07-18 18:08:07 +0200

    prepare project and add all assets

---
+++ lib/screens/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child:
+          Center(child: Image.asset("assets/Images/img-placeholder-user1.png")),
+    );
+  }
+}
